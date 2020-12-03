a6f8fe7973da0dd40bbd190d34efd9c69f8bd288
Tim Boldt <tim.boldt@gmail.com>
2020-12-02 21:35:33 -0800

    Finished template setup

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,7 @@
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
-    }
-}
+use aoc_runner_derive::aoc_lib;
+
+// pub mod day1;
+// pub mod day2;
+// pub mod day3;
 
 aoc_lib!{ year = 2020 }
