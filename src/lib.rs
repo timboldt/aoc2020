37611e571b91eb332165681c34e2128d2381270a
Tim Boldt <tim.boldt@gmail.com>
2020-12-04 20:06:29 -0800

    Rename for future expansion

--- src/lib.rs
+++ src/lib.rs
@@ -4,8 +4,8 @@ extern crate regex;
 
 use aoc_runner_derive::aoc_lib;
 
-pub mod day1;
-pub mod day2;
-// pub mod day3;
+pub mod day01;
+pub mod day02;
+// pub mod day03;
 
 aoc_lib! { year = 2020 }
