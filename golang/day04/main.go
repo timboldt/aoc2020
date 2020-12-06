a44e1883d57f8361206da2105c7135a5f9b57f2e
Tim Boldt <tim.boldt@gmail.com>
2020-12-06 15:26:14 -0800

    More cleanup in attempt to find bug

--- golang/day04/main.go
+++ golang/day04/main.go
@@ -19,24 +19,15 @@ var (
 
 func init() {
 	validRegex = map[string]*regexp.Regexp{
-		"byr": regexp.MustCompile(`^(19[2-9][0-9])|(200[0-2])$`),
-		"iyr": regexp.MustCompile(`^(201[0-9])|2020$`),
-		"eyr": regexp.MustCompile(`^(202[0-9])|2030$`),
-		"hgt": regexp.MustCompile(`^(((1[5-8][0-9])|(19[0-3]))cm)|((59|(6[0-9])|(7[0-6]))in)$`),
-		"hcl": regexp.MustCompile(`^\#[0-9a-f]{6}$`),
+		"byr": regexp.MustCompile(`^19[2-9][0-9]|200[0-2]$`),
+		"iyr": regexp.MustCompile(`^20(1[0-9]|20)$`),
+		"eyr": regexp.MustCompile(`^20(2[0-9]|30)$`),
+		"hgt": regexp.MustCompile(`^1([5-8][0-9]|9[0-3])cm|(59|6[0-9]|7[0-6])in$`),
+		"hcl": regexp.MustCompile(`^#[0-9a-f]{6}$`),
 		"ecl": regexp.MustCompile(`^amb|blu|brn|gry|grn|hzl|oth$`),
 		"pid": regexp.MustCompile(`^[0-9]{9}$`),
+		"cid": regexp.MustCompile(`.*`),
 	}
-	// 	byr (Birth Year) - four digits; at least 1920 and at most 2002.
-	// iyr (Issue Year) - four digits; at least 2010 and at most 2020.
-	// eyr (Expiration Year) - four digits; at least 2020 and at most 2030.
-	// hgt (Height) - a number followed by either cm or in:
-	// If cm, the number must be at least 150 and at most 193.
-	// If in, the number must be at least 59 and at most 76.
-	// hcl (Hair Color) - a # followed by exactly six characters 0-9 or a-f.
-	// ecl (Eye Color) - exactly one of: amb blu brn gry grn hzl oth.
-	// pid (Passport ID) - a nine-digit number, including leading zeroes.
-	// cid (Country ID) - ignored, missing or not.
 }
 
 func (p *passport) isValid1() bool {
@@ -51,7 +42,7 @@ func (p *passport) isValid1() bool {
 
 func (p *passport) isValid2() bool {
 	for rk, rv := range validRegex {
-		//fmt.Printf("%v:%v\t\t%v\n", rk, p.kv[rk], rv.MatchString(p.kv[rk]))
+		// fmt.Printf("%v\t%v:%v\n", rv.MatchString(p.kv[rk]), rk, p.kv[rk])
 		if !rv.MatchString(p.kv[rk]) {
 			return false
 		}
@@ -77,7 +68,14 @@ func parseInput(r io.Reader) ([]*passport, error) {
 		}
 		for _, f := range strings.Fields(scanner.Text()) {
 			s := strings.Split(f, ":")
-			kv[s[0]] = s[1]
+			if len(s) == 2 && validRegex[s[0]] != nil {
+				if kv[s[0]] == "" {
+					kv[s[0]] = s[1]
+				} else {
+					// Duplicate field
+					kv[s[0]] = "****"
+				}
+			}
 		}
 	}
 	if kv != nil {
